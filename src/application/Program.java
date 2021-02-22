8eabb22366085f3d1f1351a7c32ffa241924944f
Pedro Henrique Sotero <pedrupping@outlook.com>
2021-02-22 20:15:35 -0300

    Camada Chess e impressão do tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,14 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println(new Position(2, 4));
-
+		
+		ChessMatch chessMatch = new ChessMatch();
+		
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
