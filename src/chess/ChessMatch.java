ec59ccfb6554c1a6c1b653a523144431ae3d22d1
Pedro Henrique Sotero <pedrupping@outlook.com>
2021-02-23 18:29:05 -0300

    Criação e implementação BoardException

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -31,5 +31,6 @@ public class ChessMatch {
 		board.placePiece(new Rook(board, Color.BLACK), new Position(0, 7));
 		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
 		board.placePiece(new Rook(board, Color.BLACK), new Position(7, 7));
+		board.placePiece(new Rook(board, Color.BLACK), new Position(4, 6));
 	}
 }
