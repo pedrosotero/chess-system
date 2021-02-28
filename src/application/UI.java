0a55e3747a5d3b391290bfad51f7b6dac8823314
Pedro Henrique Sotero <pedrupping@outlook.com>
2021-02-27 22:15:11 -0300

    Logica de cheque

--- src/application/UI.java
+++ src/application/UI.java
@@ -44,6 +44,9 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 
 	private static void printBoard(ChessPiece[][] pieces) {
